4964bfa5a3160504c151d8e2e39547ee050b7f0e
Kevin Yen <kevin.yen@liferay.com>
2016-02-01 20:14:54 -0800

    Add command line options that allow the user to input password for remote access

--- src/main/java/me/khyen/test/JenkinsStatus.java
+++ src/main/java/me/khyen/test/JenkinsStatus.java
@@ -1,5 +1,7 @@
 package me.khyen.jenkins;
 
+import java.io.Console;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
@@ -12,21 +14,59 @@ import java.util.concurrent.Future;
 import java.util.concurrent.CompletionService;
 import java.util.concurrent.ExecutorCompletionService;
 
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.Options;
+
 /**
  * @author Kevin Yen
  */
 public class JenkinsStatus {
 
 	public static void main(String [] args) throws Exception {
+		CommandLineParser parser = new DefaultParser();
+
+		Options options = new Options();
+
+		options.addOption("u", "user", true, "Specify the username used in authentication");
+
+		CommandLine line = parser.parse(options, args);
+
+		JsonGetter jsonGetter = new LocalJsonGetter();
+
+		boolean remote = false;
+
+		if (line.hasOption("u")) {
+			Console console = System.console();
+
+			if (console == null) {
+				System.out.println("Unable to get Console instance");
+				System.exit(0);
+			}
+
+			String password = new String(console.readPassword("Enter host password: "));
+
+			String username = line.getOptionValue("u");
+
+			jsonGetter = new RemoteJsonGetter(username, password);
+
+			remote = true;
+		}
+
+		List<String> pullRequestJobURLs = JenkinsProperties.getPullRequestJobURLs(remote);
+
 		Set<Future<List<String>>> futures = new HashSet<Future<List<String>>>();
 
-		ExecutorService executor = Executors.newFixedThreadPool(30);
+		int threadPoolSize = 120;
+
+		ExecutorService executor = Executors.newFixedThreadPool(threadPoolSize);
 
 		CompletionService<List<String>> completionService = new ExecutorCompletionService<List<String>>(executor);
 
-		for (String pullRequestJobURL : JenkinsProperties.getPullRequestJobURLs()) {
-			JsonGetter jsonGetter = new LocalJsonGetter();
+		System.out.println("Checking " + pullRequestJobURLs.size() + " URLs using with a thead pool size of " + threadPoolSize);
 
+		for (String pullRequestJobURL : pullRequestJobURLs) {
 			Callable<List<String>> callable = new ActiveBuildURLsGetter(jsonGetter, pullRequestJobURL);
 
 			futures.add(completionService.submit(callable));
@@ -42,6 +82,8 @@ public class JenkinsStatus {
 			List<String> activeBuildURLs = completedFuture.get();
 
 			activePullRequestURLs.addAll(activeBuildURLs);
+
+			System.out.println(futures.size() + " threads still active");
 		}
 
 		executor.shutdown();
