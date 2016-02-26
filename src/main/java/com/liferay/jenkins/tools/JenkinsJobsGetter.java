1070c763c90ae13bdd8a88e1ac839ddd3fe4ba06
Kevin Yen <kevin.yen@liferay.com>
2016-02-25 23:19:09 -0800

    Seperate getJenkinsJob into additional methods

--- src/main/java/com/liferay/jenkins/tools/JenkinsJobsGetter.java
+++ src/main/java/com/liferay/jenkins/tools/JenkinsJobsGetter.java
@@ -40,25 +40,44 @@ public class JenkinsJobsGetter implements Callable<Set<JenkinsJob>> {
 		return getJenkinsJobs(jsonGetter, jenkinsURL);
 	}
 
-	public static Set<JenkinsJob> getJenkinsJobs(JsonGetter jsonGetter, String jenkinsURL)
-		throws Exception {
+	public static Set<JenkinsJob> getJenkinsJobs(JsonGetter jsonGetter, String jenkinsURL) throws Exception {
+		StringBuilder sb = new StringBuilder();
 
+		sb.append(jenkinsURL);
+		sb.append("api/json?tree=jobs[name,url]");
+
+		JSONObject rootJson = jsonGetter.getJson(sb.toString());
+
+		return getJenkinsJobs(rootJson);
+	}
+
+	public static Set<JenkinsJob> getJenkinsJobs(JSONObject rootJson) throws Exception {
 		Set<JenkinsJob> jenkinsJobs = new HashSet<>();
 
-		JSONObject rootJson = jsonGetter.getJson(jenkinsURL + "api/json?tree=jobs[name,url]");
+		for (JSONObject jobJson : getJobJsons(rootJson)) {
+			jenkinsJobs.add(getJenkinsJob(jobJson));
+		}
+
+		return jenkinsJobs;
+	}
+
+	public static JenkinsJob getJenkinsJob(JSONObject jobJson) throws Exception {
+			String name = jobJson.getString("name");
+			String url = jobJson.getString("url");
+
+			return new JenkinsJob(name, url);
+	}
+
+	public static Set<JSONObject> getJobJsons(JSONObject rootJson) throws Exception {
+		Set<JSONObject> jobJsons = new HashSet<>();
 
 		JSONArray jobsJson = rootJson.getJSONArray("jobs");
 
 		for (int i = 0; i < jobsJson.length(); i++) {
-			JSONObject jobJson = jobsJson.getJSONObject(i);
-
-			String name = jobJson.getString("name");
-			String url = jobJson.getString("url");
-
-			jenkinsJobs.add(new JenkinsJob(name, url));
+			jobJsons.add(jobsJson.getJSONObject(i));
 		}
 
-		return jenkinsJobs;
+		return jobJsons;
 	}
 
 }
