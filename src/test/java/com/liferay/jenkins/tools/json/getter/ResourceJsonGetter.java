f49599803c6948a1e67ab5b07ac3c67cf4f8bbdf
Kevin Yen <kevin.yen@liferay.com>
2016-05-14 01:18:49 -0700

    Add ResourceJsonGetter class

---
+++ src/test/java/com/liferay/jenkins/tools/json/getter/ResourceJsonGetter.java
@@ -0,0 +1,55 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.jenkins.tools;
+
+import java.io.IOException;
+
+import java.net.URL;
+
+import java.nio.charset.Charset;
+
+import org.apache.commons.io.IOUtils;
+
+import org.json.JSONObject;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author Kevin Yen
+ */
+public class ResourceJsonGetter implements JsonGetter {
+
+	private static final Logger logger = LoggerFactory.getLogger(ResourceJsonGetter.class);
+
+	@Override
+	public String convertURL(String url) {
+		return url;
+	}
+
+	@Override
+	public JSONObject getJson(String file) throws Exception {
+		URL fileURL = ResourceJsonGetter.class.getResource(file);
+
+		if (fileURL == null) {
+			throw new IOException(file + " not found");
+		}
+
+		logger.debug("Retrieving json resource from {}", file);
+
+		return new JSONObject(IOUtils.toString(fileURL.openStream(), Charset.defaultCharset()));
+	}
+
+}
