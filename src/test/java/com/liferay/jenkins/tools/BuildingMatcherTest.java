d42d9022533458fcdc2bdacb1010598814e7ac17
Kevin Yen <kevin.yen@liferay.com>
2016-05-14 01:36:42 -0700

    Add more unit tests for BuildingMatcher

--- src/test/java/com/liferay/jenkins/tools/BuildingMatcherTest.java
+++ src/test/java/com/liferay/jenkins/tools/BuildingMatcherTest.java
@@ -18,10 +18,48 @@ import static org.junit.Assert.*;
 
 import org.junit.*;
 
+import org.json.JSONObject;
+
 /**
  * @author Kevin Yen
  */
 public class BuildingMatcherTest {
+
+	JsonGetter resourceJsonGetter = new ResourceJsonGetter();
+
+	Build build;
+
+	@Before
+	public void setup() throws Exception {
+		JSONObject jobJson = resourceJsonGetter.getJson("/json-samples/job.json");
+		JSONObject buildJson = resourceJsonGetter.getJson("/json-samples/build.json");
+
+		buildJson.put("building", "false");
+
+		Job job = new Job(jobJson);
+		build = new Build(buildJson, job);
+	}
+
+	@Test
+	public void testNotMatchesBuildingTrue() {
+		BuildMatcher buildMatcher = new BuildingMatcher("true");
+
+		assertFalse(buildMatcher.matches(build));	
+	}
+
+	@Test
+	public void testMatchesBuildingFalse() {
+		BuildMatcher buildMatcher = new BuildingMatcher("false");
+
+		assertTrue(buildMatcher.matches(build));	
+	}
+
+	@Test
+	public void testMatchesBuildingAny() {
+		BuildMatcher buildMatcher = new BuildingMatcher("false");
+
+		assertTrue(buildMatcher.matches(build));	
+	}
 
 	@Test
 	public void ConstructorValidArgumentAny() {
